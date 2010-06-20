914c93d004e10c25fc15591b9fd92b62d26c6d7e
snakebyteme2 <snakebyteme2@b007d3a0-da23-09f3-5b22-cc9422e92750>
2010-06-20 04:10:06 +0000

    added BASS as the sound library. Added ability to change the key. Made CDG output a separate window. Added double click on CDG output window to maximize/return to normal Added .zip support

---
+++ trunk/CDGWindow.vb
@@ -0,0 +1,22 @@
+﻿Public Class CDGWindow
+
+  Private Sub CDGWindow_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.DoubleClick
+    AutoSizeWindow()
+  End Sub
+
+  Private Sub PictureBox1_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles PictureBox1.DoubleClick
+    AutoSizeWindow()
+  End Sub
+
+  Private Sub AutoSizeWindow()
+    If Me.WindowState = FormWindowState.Normal Then
+      Me.WindowState = FormWindowState.Maximized
+      Me.FormBorderStyle = Windows.Forms.FormBorderStyle.None
+      Me.Refresh()
+    Else
+      Me.WindowState = FormWindowState.Normal
+      Me.FormBorderStyle = Windows.Forms.FormBorderStyle.Sizable
+      Me.Refresh()
+    End If
+  End Sub
+End Class
