1c951692c9654d3a0258e6867247760f793a2c63
snakebyteme2 <snakebyteme2@b007d3a0-da23-09f3-5b22-cc9422e92750>
2010-06-26 00:38:13 +0000

    Added Movie and Graphic background options to the CDG converter.

---
+++ trunk/MP3+CDG To Video Converter/MovieFrameExtractor.vb
@@ -0,0 +1,29 @@
+﻿Imports DexterLib
+Imports System.Drawing.Imaging
+Imports System.Runtime.InteropServices
+Imports System.IO
+
+Public Class MovieFrameExtractor
+
+  Public Shared Function GetBitmap(ByVal position As Double, ByVal movieFileName As String, ByVal width As Integer, ByVal height As Integer) As Bitmap
+
+    Dim det As New DexterLib.MediaDetClass()
+    det.Filename = movieFileName
+    det.CurrentStream = 0
+    Dim len As Double = det.StreamLength
+    If position > len Then
+      Return Nothing
+    End If
+
+    Dim myTempFile As String = System.IO.Path.GetTempFileName
+    det.WriteBitmapBits(position, width, height, myTempFile)
+    Dim myBMP As Bitmap
+    Using lStream As New FileStream(myTempFile, FileMode.Open, FileAccess.Read)
+      myBMP = Image.FromStream(lStream)
+    End Using
+    System.IO.File.Delete(myTempFile)
+    Return myBMP
+
+  End Function
+
+End Class
